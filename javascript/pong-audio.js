3c3521a8964fb60d3be3e44db2f91939c54ce75f
Trippinteddy <83976342+Trippinteddy@users.noreply.github.com>
2023-01-04 12:51:03 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -55,7 +55,7 @@ soundArray.push(scoreSound);
 export var ambientSound = new soundFile("Autum.mp3");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
-ambientSound.player.volume.value = 0; //turn down volume
+ambientSound.player.volume.value = 15; //turn down volume
 
 export var adventureMusic = new soundFile("Autum.mp3");
 soundArray.push(adventureMusic);
